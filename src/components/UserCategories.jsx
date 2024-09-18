236bf1621c44cc0d3a278dfcbe3040d8b29b3338
Ramji-Dev <ramjirajputkd52@gmail.com>
2024-09-19 00:12:35 +0530

    minor fix

--- src/components/UserCategories.jsx
+++ src/components/UserCategories.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function UserCategories({name, number}) {
   return (
     <div>
-        <div onClick={() => setIsActive(!isActive)}>
+        <div>
             <div className={`w-fit flex items-center justify-center gap-[0.8vw] px-[0.8vw] py-[0.2vw] border-2 border-[#5e3a7a] rounded-md capitalize text-[0.8vw] font-inter font-semibold ${name==="All" ? "bg-[#5e3a7a] text-white" : "text-[#5e3a7a]"}`}>
                 <span>
                     {name}
