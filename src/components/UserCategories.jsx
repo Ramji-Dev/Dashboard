6f935e82e9fcb9d645cadb6ea7b31c212a98ce9e
Ramji-Dev <ramjirajputkd52@gmail.com>
2024-09-18 21:42:21 +0530

    minor fix

--- src/components/UserCategories.jsx
+++ src/components/UserCategories.jsx
@@ -4,7 +4,7 @@ function UserCategories({name, number}) {
   return (
     <div>
         <div onClick={() => setIsActive(!isActive)}>
-            <div className={`w-fit flex items-center justify-center gap-[0.8vw] px-[0.8vw] py-[0.2vw] border-2 border-[#5e3a7a] rounded-md capitalize text-[0.6vw] font-inter font-semibold ${name==="All" ? "bg-[#5e3a7a] text-white" : "text-[#5e3a7a]"}`}>
+            <div className={`w-fit flex items-center justify-center gap-[0.8vw] px-[0.8vw] py-[0.2vw] border-2 border-[#5e3a7a] rounded-md capitalize text-[0.8vw] font-inter font-semibold ${name==="All" ? "bg-[#5e3a7a] text-white" : "text-[#5e3a7a]"}`}>
                 <span>
                     {name}
                 </span>
